232b70b6c24c23e3c8fd35ca1d01f28bdc4d2fad
Priyanka Malviya <priyanka_malviya@icloud.com>
2016-08-16 14:54:59 -0700

    authentication methods

--- client/components/signup/signupform.jsx
+++ client/components/signup/signupform.jsx
@@ -1,4 +1,36 @@
 Signupform = React.createClass({
+    mixins: [ReactMeteorData],
+    getMeteorData(){
+        let data={};
+        data.currentUser = Meteor.user();
+        return data;
+    },
+    getInitialState(){
+        return {
+            message:'',
+            messageClass: ''
+        }
+    },
+    displayError(message){
+        this.setState({message:message, messageClass: 'alert alert-danger registerError'});
+        this.setState({message:message, messageClass: 'alert alert-danger registerError'});
+    },
+    handleSubmit(e){
+        e.preventDefault();
+        this.setState({message:'', messageClass:'hidden'});
+        var that = this;
+        var first_name = ReactDOM.findDOMNode(this.refs.first_name).value.trim();
+        var last_name = ReactDOM.findDOMNode(this.refs.last_name).value.trim();
+        var email = ReactDOM.findDOMNode(this.refs.email).value.trim();
+        var password = ReactDOM.findDOMNode(this.refs.password).value.trim();
+        var user = {email: email, password: password, profile:{fullname : (first_name + last_name).toLowerCase(), firstname: first_name, lastname: last_name, avatar: "http://placehold.it/150x150", friends: []}}
+        Accounts.createUser(user, function(e){
+            FlowRouter.go('/dashboard');
+            if(e){
+                that.displayError(e.reason);
+            }
+        });
+    },
     render(){
         return(
             <div className="row pull-right">
@@ -8,7 +40,7 @@ Signupform = React.createClass({
                         It'r free and always will be.
                     </p>
                 </div>
-                <form >
+                <form onSubmit={this.handleSubmit}>
                     <div className="col-sm-9">
                         <div className="row">
                             <div className="col-sm-6 form-group">
@@ -25,6 +57,7 @@ Signupform = React.createClass({
                             </div>
                         </div>
                         <button className="btn btn-success btn-md">Sign Up</button>
+                        <span className={this.state.messageClass}>{this.state.message}</span>
                     </div>
                 </form>
             </div>
